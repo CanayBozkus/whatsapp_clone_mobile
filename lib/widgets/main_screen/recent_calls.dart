147d959b1ff11283ffaa7b4f5d1e8a10881719f0
canaybozkus <bozkuscanay@gmail.com>
2021-05-28 20:26:45 +0300

    base recent activity card, recent chat card using activity card, recent call card using activity vard

--- lib/widgets/main_screen/recent_calls.dart
+++ lib/widgets/main_screen/recent_calls.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone_mobile/widgets/main_screen/recent_call_card.dart';
 
 class RecentCalls extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('calls'),
+    return ListView(
+      children: [
+        RecentCallCard(),
+        RecentCallCard(isVideoCall: true,),
+      ],
     );
   }
 }
--- lib/widgets/main_screen/recent_chat_card.dart
+++ lib/widgets/main_screen/recent_chat_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone_mobile/screens/chat_screen.dart';
 import 'package:whatsapp_clone_mobile/utilities/constants.dart';
+import 'package:whatsapp_clone_mobile/widgets/main_screen/base_recent_activity_card.dart';
 
 class RecentChatCard extends StatelessWidget {
   const RecentChatCard({this.showTime = true, this.isMuted});
@@ -13,120 +14,57 @@ class RecentChatCard extends StatelessWidget {
     return 75;
   }
 
+
   @override
   Widget build(BuildContext context) {
-    return InkWell(
+    return BaseRecentActivityCard(
       onTap: (){
         Navigator.pushNamed(context, ChatScreen.routeName);
       },
-      onLongPress: (){
-
-      },
-      child: Container(
-        height: 80,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              width: 90,
-              alignment: Alignment.center,
-              child: CircleAvatar(
-                radius: 30,
-                child: ClipOval(
-                  child: Image.asset('assets/images/avatar.png'),
-                ),
-              ),
+      onLongPress: (){},
+      mainText: 'Canay Bozkuş',
+      subText: 'naber? nasılsın',
+      rightContainerWidth: getTimeContainerWidth(),
+      rightContainerChild: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          SizedBox(height: 16,),
+          Text(
+            showTime ? '17:01' : '01:01:2000',
+            style: TextStyle(
+                color: Colors.white70,
+                fontSize: 14
             ),
-            Expanded(
-              child: Container(
+          ),
+          SizedBox(height: 10,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              isMuted ? Icon(
+                Icons.volume_off,
+                color: Colors.white70,
+              ) : SizedBox.shrink(),
+              SizedBox(width: 5,),
+              Container(
+                padding: EdgeInsets.all(8),
                 decoration: BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      color: Colors.white38,
-                      width: 0.5,
-                    )
-                  )
+                  color: Constant.lightPrimaryColor,
+                  shape: BoxShape.circle,
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 12,),
-                    Text(
-                      'Canay Bozkuş',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: Constant.defaultFontSize,
-                          fontWeight: FontWeight.w500
-                      ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    SizedBox(height: 10,),
-                    Text(
-                      'https://www.producthunt.com/discussions?ref=header_nav',
-                      style: TextStyle(
-                        color: Colors.white70,
-                        fontSize: 14,
-                      ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              width: getTimeContainerWidth(),
-              decoration: BoxDecoration(
-                border: Border(
-                    bottom: BorderSide(
-                      color: Colors.white38,
-                      width: 0.5,
-                    )
-                ),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  SizedBox(height: 16,),
-                  Text(
-                    showTime ? '17:01' : '01:01:2000',
-                    style: TextStyle(
-                        color: Colors.white70,
-                        fontSize: 14
-                    ),
+                child: Text(
+                  '1',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.white,
                   ),
-                  SizedBox(height: 10,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      isMuted ? Icon(
-                        Icons.volume_off,
-                        color: Colors.white70,
-                      ) : SizedBox.shrink(),
-                      SizedBox(width: 5,),
-                      Container(
-                        padding: EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          color: Constant.lightPrimaryColor,
-                          shape: BoxShape.circle,
-                        ),
-                        child: Text(
-                          '1',
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 5,),
-                    ],
-                  ),
-                ],
+                ),
               ),
-            ),
-            SizedBox(width: 10,),
-          ],
-        ),
+              SizedBox(width: 5,),
+            ],
+          ),
+        ],
       ),
     );
   }
 }
+
