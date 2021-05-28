0cf99cdc64a2f3a075c4f1de268a80f7737067f3
canaybozkus <bozkuscanay@gmail.com>
2021-05-28 14:11:06 +0300

    chat screen text input

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone_mobile/utilities/constants.dart';
 import 'package:whatsapp_clone_mobile/widgets/chat_screen/chat_screen_appbar.dart';
+import 'package:whatsapp_clone_mobile/widgets/form/base_textformfield.dart';
 
 class ChatScreen extends StatelessWidget {
   static const routeName = 'ChatScreen';
@@ -14,7 +15,102 @@ class ChatScreen extends StatelessWidget {
           body: Column(
             children: [
               ChatScreenAppBar(),
-              SizedBox(height: 50,),
+              Expanded(
+                child: ListView(
+                  children: [],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 5),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: BaseTextFormField(
+                        maxLine: 3,
+                        borderRadius: 20,
+                        hintText: 'Type a message',
+                        prefixIcon: Material(
+                          color: Colors.transparent,
+                          child: IconButton(
+                            splashRadius: 20,
+                            icon: Icon(
+                              Icons.tag_faces,
+                              color: Colors.white,
+                            ),
+                            onPressed: (){
+                              print(1);
+                            },
+                          ),
+                        ),
+                        suffixIcon: Container(
+                          width: 65,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              SizedBox(
+                                width: 30,
+                                child: Material(
+                                  color: Colors.transparent,
+                                  child: IconButton(
+                                    padding: EdgeInsets.zero,
+                                    splashRadius: 15,
+                                    iconSize: 20,
+                                    icon: Icon(
+                                      Icons.attach_file,
+                                      color: Colors.white,
+                                    ),
+                                    onPressed: (){
+                                      print(1);
+                                    },
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 30,
+                                child: Material(
+                                  color: Colors.transparent,
+                                  child: IconButton(
+                                    iconSize: 20,
+                                    padding: EdgeInsets.zero,
+                                    splashRadius: 15,
+                                    icon: Icon(
+                                      Icons.camera_alt,
+                                      color: Colors.white,
+                                    ),
+                                    onPressed: (){
+                                      print(1);
+                                    },
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: 5,),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 5,),
+                    ClipOval(
+                      child: Material(
+                        color: Constant.primaryColor,
+                        shape: CircleBorder(
+                          side: BorderSide(
+                            color: Colors.black,
+                            width: 2
+                          )
+                        ),
+                        child: IconButton(
+                          icon: Icon(Icons.send, color: Colors.white,),
+                          onPressed: (){},
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 5,),
             ],
           ),
         ),
--- lib/widgets/form/base_textformfield.dart
+++ lib/widgets/form/base_textformfield.dart
@@ -2,19 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:whatsapp_clone_mobile/utilities/constants.dart';
 
 class BaseTextFormField extends StatelessWidget {
-  BaseTextFormField({this.hintText = ''});
+  BaseTextFormField({
+    this.hintText = '',
+    this.borderRadius = 6.0,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.minLine = 1,
+    this.maxLine = 1,
+  });
 
   final String hintText;
+  final double borderRadius;
+  final Widget prefixIcon;
+  final Widget suffixIcon;
+  final int minLine;
+  final int maxLine;
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
     return TextFormField(
+      maxLines: maxLine,
+      minLines: minLine,
       style: TextStyle(
-        color: Colors.white70,
+        color: Colors.white,
         letterSpacing: 0.75,
         fontSize: Constant.defaultFontSize,
       ),
       decoration: InputDecoration(
+        prefixIcon: prefixIcon,
+        suffixIcon: suffixIcon,
         hintText: hintText,
         hintStyle: TextStyle(
           color: Colors.white70,
@@ -24,15 +40,15 @@ class BaseTextFormField extends StatelessWidget {
         ),
         fillColor: theme.primaryColor,
         filled: true,
-        contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+        contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: suffixIcon != Null ? 0 : 15),
         enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(6.0),
+          borderRadius: BorderRadius.circular(borderRadius),
           borderSide: BorderSide(
               width: 2
           ),
         ),
         focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(6.0),
+          borderRadius: BorderRadius.circular(borderRadius),
           borderSide: BorderSide(
               color: Constant.lightPrimaryColor,
               width: 2
