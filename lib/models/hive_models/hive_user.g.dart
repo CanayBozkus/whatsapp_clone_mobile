ee922433b758c0e10c2000cb555a4b70942ed961
canaybozkus <bozkuscanay@gmail.com>
2021-05-31 16:28:41 +0300

    hive_user.g.dart

---
+++ lib/models/hive_models/hive_user.g.dart
@@ -0,0 +1,55 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'hive_user.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class HiveUserAdapter extends TypeAdapter<HiveUser> {
+  @override
+  final int typeId = 0;
+
+  @override
+  HiveUser read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return HiveUser()
+      ..phoneNumber = fields[0] as String
+      ..id = fields[1] as String
+      ..name = fields[2] as String
+      ..contacts = (fields[3] as List)?.cast<dynamic>()
+      ..profilePictureName = fields[4] as String
+      ..showLastSeen = fields[5] as int;
+  }
+
+  @override
+  void write(BinaryWriter writer, HiveUser obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.phoneNumber)
+      ..writeByte(1)
+      ..write(obj.id)
+      ..writeByte(2)
+      ..write(obj.name)
+      ..writeByte(3)
+      ..write(obj.contacts)
+      ..writeByte(4)
+      ..write(obj.profilePictureName)
+      ..writeByte(5)
+      ..write(obj.showLastSeen);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is HiveUserAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
